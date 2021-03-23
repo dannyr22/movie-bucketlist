bf69a76dfb258d4090d9604e75f7b904607941ff
dannyr22 <dannyr2763@hotmail.com>
2021-03-23 18:04:47 +0000

    intial commit

---
+++ src/components/MovieList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MovieList = () => {
+  return (
+    <div>
+      <ul className="movielist">
+        <li>The Constant Gardener</li>
+        <li>The Fountain</li>
+        <li>Mystic River</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MovieList
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Movie BucketList</h1>
+      <ul className="nav">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact Us</li>
+        <li>Recommend a Movie</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
